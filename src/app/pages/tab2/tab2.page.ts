d38c2c32d7c85c7697af0b2bbdd6d66e5253ef08
Makina Dev <v.hauchecorne@outlook.fr>
2021-12-01 18:54:39 +0400

    Ecran nouvelle question : Ajustement de la position des boutons

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public questionsType;
+  public difficulties;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.questionsType = this.getQuestionType();
+    this.difficulties = this.getDifficulties();
+  }
+
+  public getQuestionType() {
+    return ["Question", "QCM", "Lexical", "Chaudron", "Débat", "Speech", "Gage", "Vrai ou faux", "Affirmation"];
+  }
+
+  public getDifficulties(){
+    return  ["FACILE", "MOYEN", "DIFFICILE"];
+  }
+
+  public questionTypeChange(event){
+    console.log(event);
+  }
+
+  public difficultyChange(event){
+    console.log(event)
+  }
 
 }
