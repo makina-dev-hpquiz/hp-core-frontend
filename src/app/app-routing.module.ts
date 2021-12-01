48b0ffd5f217d01502ba468d8b8aa8ee00ba2c34
Makina Dev <v.hauchecorne@outlook.fr>
2021-12-01 16:23:38 +0400

    Creation page home, toutes les pages sont maintenant dans /pages/home, la page home est maintenant la page principale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
+ 
 ];
 @NgModule({
   imports: [
