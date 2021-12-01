081d79439f51e8c4692b392a17d633ce20ed1124
Makina Dev <v.hauchecorne@outlook.fr>
2021-12-01 18:03:54 +0400

    Creation de la page home en mode static

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,7 +9,54 @@ export class HomePage implements OnInit {
 
   constructor() { }
 
+  public questions;
+
+  public difficulties;
+  public selectedDifficulty;
+  public difficultyColor = "green";
+
+
   ngOnInit() {
+    this.questions = [{"nb" : 13, "type" : "Question"},
+    {"nb" : 3, "type" : "QCM"},
+    {"nb" : 2, "type" : "Lexical"},
+    {"nb" : 1, "type" : "Chaudron"},
+    {"nb" : 2, "type" : "Débat"},
+    {"nb" : 1, "type" : "Speech"},
+    {"nb" : 3, "type" : "Gage"},
+    {"nb" : 3, "type" : "Vrai ou faux"},
+    {"nb" : 3, "type" : "Affirmation"}                    
+  ];
+
+
+  this.difficulties = ["FACILE", "MOYEN", "DIFFICILE"];
+  this.selectedDifficulty = this.difficulties[0];
+  }
+
+  public countNbOfAnswersForSelectedDifficulty(){
+    var totalNbOfAnswers = 0;
+   if(this.questions != undefined) {
+    this.questions.forEach(question => {
+      totalNbOfAnswers += question.nb;
+    });
+  }
+
+    return totalNbOfAnswers;
+  }
+
+  public difficultyChange(event){
+    this.selectedDifficulty = event.detail.value;
+    switch(this.selectedDifficulty){
+      case "FACILE" : 
+        this.difficultyColor = "green";
+      break;
+      case "MOYEN" : 
+        this.difficultyColor = "orange";
+      break;
+      case "DIFFICILE" : 
+        this.difficultyColor = "red";
+      break;
+    }
   }
 
 }
