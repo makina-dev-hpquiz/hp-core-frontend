e6621ffe8c3ae182ec84db048ac3f8bd711c870c
Makina Dev <v.hauchecorne@outlook.fr>
2021-12-02 12:16:17 +0400

    Creation en static de l'écran Liste questions

--- src/app/pages/listAnswers/listAnswers.page.ts
+++ src/app/pages/listAnswers/listAnswers.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-Answers',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class ListAnswersPage {
 
-  constructor() {}
+  public question;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(){
+    this.question = {"question": "Quel est le numéro du coffre où Hagrid récupère un mystérieux paquet?", "type":"question"};
+  }
+
+  public goToDetail(){
+    this.router.navigate(['/tabs/detail']);
+    //Service stockage data avec transition de page
+  }
+
+
+  public getMiniNameType(typeName: String) {
+    return typeName.substring(0, 1).toUpperCase();
+  }
 }
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
       {
         path: 'new',
         loadChildren: () => import('../newAnswer/newAnswer.module').then(m => m.NewAnswerPageModule)
+      }, 
+      {
+        path: 'detail',
+        loadChildren: () => import('../newAnswer/newAnswer.module').then(m => m.NewAnswerPageModule)
       },
       {
         path: 'groups',
