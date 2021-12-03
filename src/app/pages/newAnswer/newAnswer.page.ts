70aa324e981787d3e6d1b5e7527880e703fea243
Makina Dev <v.hauchecorne@outlook.fr>
2021-12-03 17:48:27 +0400

    Ecran new question : Ajout des inputs

--- src/app/pages/newAnswer/newAnswer.page.ts
+++ src/app/pages/newAnswer/newAnswer.page.ts
@@ -7,26 +7,47 @@ import { Component } from '@angular/core';
 })
 export class NewAnswerPage {
 
+  public QUESTION = "Question";
+  public QCM = "QCM";
+  public LEXICAL = "Lexical";
+  public CHAUDRON = "Chaudron";
+  public DEBAT = "Débat";
+  public SPEECH = "Speech";
+  public GAGE = "Gage";
+  public VRAI_OU_FAUX = "Vrai ou faux";
+  public AFFIRMATION = "Affirmation";
+
+  public FACILE = "FACILE";
+  public MOYEN = "MOYEN";
+  public DIFFICILE = "DIFFICILE";
+  
   public questionsType;
+  public selectedQuestion : String;
+  public selectedDifficulty : String;
   public difficulties;
+
+
 
   constructor() { }
 
   ngOnInit() {
     this.questionsType = this.getQuestionType();
     this.difficulties = this.getDifficulties();
+
+    this.selectedQuestion = this.questionsType[0];
+    this.selectedDifficulty = this.MOYEN;
   }
 
   public getQuestionType() {
-    return ["Question", "QCM", "Lexical", "Chaudron", "Débat", "Speech", "Gage", "Vrai ou faux", "Affirmation"];
+    return [this.QUESTION, this.QCM, this.LEXICAL, this.CHAUDRON, this.DEBAT, this.SPEECH, this.GAGE, this.VRAI_OU_FAUX, this.AFFIRMATION];
   }
 
   public getDifficulties(){
-    return  ["FACILE", "MOYEN", "DIFFICILE"];
+    return  [this.FACILE, this.MOYEN, this.DIFFICILE];
   }
 
   public questionTypeChange(event){
-    console.log(event);
+    this.selectedQuestion = event.detail.value;
   }
 
   public difficultyChange(event){
